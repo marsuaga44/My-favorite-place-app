b516e85ddd817fc5ce1b1fd5372d74e339e92b25
Mar <mar.arsuaga.s@gmail.com>
2016-02-22 09:18:23 -0600

    Initial Commit

---
+++ Exercise14/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Exercise14
+//
+//  Created by Marimar Arsuaga on 2/22/16.
+//  Copyright © 2016 whappif. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
